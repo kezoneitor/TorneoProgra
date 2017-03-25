dda863851a362ed44dfa6b6d148c1040bc45c594
B-CoMpLeX <julio.c3sar.castr0@gmail.com>
2017-03-25 17:02:56 -0600

    lista y get and set

--- PrograTorneo/src/progratorneo/Administrador.java
+++ PrograTorneo/src/progratorneo/Administrador.java
@@ -10,6 +10,7 @@ package progratorneo;
  * @author Kezo
  */
 public class Administrador {
+    String [] listTorneo;
     String nombre;
     String clave;
     int dinero;
@@ -20,4 +21,29 @@ public class Administrador {
         this.dinero = dinero;
     }
     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(int dinero) {
+        this.dinero = dinero;
+    }
+        
+
 }
