6e5886bd3f6f7d3b248e794421528269f5303ed1
kezoneitor <keslerth.c@gmail.com>
2017-03-25 16:49:10 -0600

    Clase administrador

---
+++ PrograTorneo/src/progratorneo/Administrador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progratorneo;
+
+/**
+ *
+ * @author Kezo
+ */
+public class Administrador {
+    String nombre;
+    String clave;
+    int dinero;
+
+    public Administrador(String nombre, String clave, int dinero) {
+        this.nombre = nombre;
+        this.clave = clave;
+        this.dinero = dinero;
+    }
+    
+}
